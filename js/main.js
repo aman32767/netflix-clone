579bfb408117cbb75e3b5cb40cb7268099226e42
Aman Sharma <writetoaman99@gmail.com>
2019-07-29 21:12:32 +0530

    finishing up

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
-//nothing yet
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+
+// Select tab content item
+function selectItem(e) {
+    // Remove all show and border classes
+    removeBorder();
+    removeShow();
+
+    this.classList.add('tab-border');
+    console.log(this.id)
+    const tabContentItem = document.querySelector(`#${this.id}-content`);
+    tabContentItem.classList.add('show');
+}
+
+// Remove bottom borders from all tab items
+function removeBorder() {
+    tabItems.forEach(item => {
+        item.classList.remove('tab-border');
+    });
+}
+
+// Remove show class from all content items
+function removeShow() {
+    tabContentItems.forEach(item => {
+        item.classList.remove('show');
+    });
+}
+
+// Listen for tab item click
+tabItems.forEach(item => {
+    item.addEventListener('click', selectItem);
+});
