c0541d6233e542caeb6421c929a247acd167b1cc
Aman Sharma <52220597+aman32767@users.noreply.github.com>
2019-07-30 19:30:30 +0530

    .

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
 const tabItems = document.querySelectorAll('.tab-item');
 const tabContentItems = document.querySelectorAll('.tab-content-item');
-
-// Select tab content item
 function selectItem(e) {
-    // Remove all show and border classes
     removeBorder();
     removeShow();
 
@@ -12,22 +9,16 @@ function selectItem(e) {
     const tabContentItem = document.querySelector(`#${this.id}-content`);
     tabContentItem.classList.add('show');
 }
-
-// Remove bottom borders from all tab items
 function removeBorder() {
     tabItems.forEach(item => {
         item.classList.remove('tab-border');
     });
 }
-
-// Remove show class from all content items
 function removeShow() {
     tabContentItems.forEach(item => {
         item.classList.remove('show');
     });
 }
-
-// Listen for tab item click
 tabItems.forEach(item => {
     item.addEventListener('click', selectItem);
 });
